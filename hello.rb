b54eda79795b0976af415a7b2590cfa2d481b565
ninjarails <ninjarailsdev@gmail.com>
2017-09-08 20:20:40 +0800

    asd

--- hello.rb
+++ hello.rb
@@ -1,8 +1,26 @@
 require 'sinatra'
+require 'pony'
+
+
 
 get '/incoming-sms' do
-  puts params['text']
-  puts params['messageId']
-  puts params['to']
-  # do something interesting with this data...
+	puts params['text']
+	puts params['messageId']
+	puts params['to']
+	# do something interesting with this data...
+		
+	Pony.mail :to => 'joellejmaze918@gmail.com',
+	          :from => params['from'],
+	          :subject =>  params['messageId'],
+	          :body =>  params['text'],
+	          :via => :smtp,
+	          :via_options => {
+	              :address              => 'smtp.gmail.com',
+	              :port                 => '587',
+	              :user_name                 => 'joellejmaze918',
+	              :password             => 'strongman',
+	              :authentication       => :plain, 
+	              :domain               => "virtual-phone-test.herokuapp.com" 
+	            } 
+})
 end
